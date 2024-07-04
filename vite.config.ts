f72b4f65d4ffd35e2845e70a241087063947933e
Ahmad Fariz <ahmadfariz.ae@gmail.com>
2024-07-04 08:49:06 +0700

    update vite config and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/simulasi-kpr/',
   plugins: [
     remix({
+      basename: '/simulasi-kpr/',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
